b426c278741d67444ccb5fadc2e96b0091d3ae23
EvsStalnova <133530610+EvsStalnova@users.noreply.github.com>
2023-07-22 23:45:02 +0300

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+from datetime import datetime
+
+
+class PostList(ListView):
+    model = Post
+    ordering = 'title'
+    template_name = 'news/articles.html'
+    context_object_name = 'Posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = 'Распродажа НИКОГДА!'
+        return context
+
+
+# Create your views here.
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'news/article.html'
+    context_object_name = 'post'
